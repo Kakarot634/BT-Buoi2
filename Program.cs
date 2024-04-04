c168a69d74f64f3b65f4fd5365b27755c8e06d51
Kakarot634 <114454554+Kakarot634@users.noreply.github.com>
2024-04-04 15:01:48 +0700

    BT Buoi2

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+
+namespace Baitapbuoi2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Hiển thị thời gian hiện tại
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is: " + localDate);
+
+            // Sử dụng toán tử 
+
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            System.Console.WriteLine("Area is: " + area);
+            
+            // Hiển thị lời chào
+            System.Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            System.Console.WriteLine("Hello " + yourName);
+
+            //Chuyển đổi tiền tệ
+            System.Console.WriteLine("Ứng dụng chuyển đổi tiền tệ");
+            double exchangeRate = 23000;
+
+            Console.Write("Nhập số tiền muốn chuyển đổi (USD): ");
+            double amountUSD = double.Parse(Console.ReadLine());
+
+            double convertedAmountBTC = amountUSD * exchangeRate;
+
+            Console.WriteLine($"{amountUSD} USD = {convertedAmountBTC} BTC");
+        }
+    }
+}
